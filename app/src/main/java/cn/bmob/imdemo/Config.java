6441310248341f78f7185aba569e9b4a040d7d18
unknown <2532758812@qq.com>
2016-03-24 13:44:33 +0800

    Initial commit

---
+++ app/src/main/java/cn/bmob/imdemo/Config.java
@@ -0,0 +1,16 @@
+package cn.bmob.imdemo;
+
+/**
+ * @author :smile
+ * @project:Config
+ * @date :2016-01-15-18:23
+ */
+public class Config {
+    /**
+     * Bmob应用key
+     */
+//    public static final String DEFAULT_APPKEY="d6f44e8f1ba9d3dcf4fab7a487fa97dd";
+    public static final String DEFAULT_APPKEY="87ab0f9bee41bce86dfadd69af692873";
+    public static final boolean DEBUG=true;
+
+}
